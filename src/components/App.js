3cd2640b6995c0a940622079099cc867038d20e5
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2020-01-01 21:06:03 +0200

    Getting JSON.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import '../assets/css/App.scss'
 import React, {Component} from 'react'
+import axios from 'axios'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.pollAll()
+  }
+
+  pollAll() {
+    const url = 'https://www.reddit.com/r/all.json'
+    axios.get(url)
+      .then(response => {
+        const posts = response.data.data.children.map(child=>child.data)
+        console.log(posts)
+      })
+      .catch(console.log)
+  }
+
   render() {
     return (
       <div>
